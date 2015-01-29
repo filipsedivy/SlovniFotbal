b592dfc0fc1b10e9e78fc6e3d70d5ce2cb269a3c
Filip Sedivy <mail@filipsedivy.cz>
2015-01-29 13:25:16 +0100

    Vytvoření vyjímky aplikace

---
+++ Core/Vyjimky.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slovní_fotbal.Core
+{
+    class SlovniFotbalException : Exception
+    {
+        public SlovniFotbalException()
+        {
+
+        }
+
+        public SlovniFotbalException(string message)
+            : base(message)
+        { }
+
+        public SlovniFotbalException(string message, Exception inner)
+            : base(message, inner)
+        { }
+    }
+}
