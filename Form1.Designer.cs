cdf08598a8404a0cb700907e183ec320a05abc8b
Filip Sedivy <mail@filipsedivy.cz>
2015-01-29 19:46:16 +0100

    Změna designu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.run = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.letters = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.Testing = new System.Windows.Forms.LinkLabel();
             this.tab = new System.Windows.Forms.TabControl();
             this.tabProgram = new System.Windows.Forms.TabPage();
             this.listBox2 = new System.Windows.Forms.ListBox();
@@ -49,10 +44,12 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.tabAbout = new System.Windows.Forms.TabPage();
-            this.actionProgress = new System.Windows.Forms.ProgressBar();
-            this.actionHelper = new System.Windows.Forms.Label();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.actionHelper = new System.Windows.Forms.ToolStripStatusLabel();
+            this.actionProgress = new System.Windows.Forms.ToolStripProgressBar();
             this.uiNastaveni = new Slovní_fotbal.Core.UI.Nastaveni();
             this.autor = new Slovní_fotbal.Core.UI.Autor();
+            this.countWords = new System.Windows.Forms.Label();
             this.tab.SuspendLayout();
             this.tabProgram.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
@@ -60,52 +57,8 @@
             this.tabLicence.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.tabAbout.SuspendLayout();
+            this.statusBar.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // run
-            // 
-            this.run.Location = new System.Drawing.Point(8, 58);
-            this.run.Name = "run";
-            this.run.Size = new System.Drawing.Size(230, 27);
-            this.run.TabIndex = 0;
-            this.run.Text = "Spustit";
-            this.run.UseVisualStyleBackColor = true;
-            this.run.Click += new System.EventHandler(this.run_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 37);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Písmena";
-            // 
-            // letters
-            // 
-            this.letters.Location = new System.Drawing.Point(62, 34);
-            this.letters.Name = "letters";
-            this.letters.Size = new System.Drawing.Size(178, 20);
-            this.letters.TabIndex = 5;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(246, 6);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(173, 56);
-            this.listBox1.TabIndex = 6;
-            // 
-            // Testing
-            // 
-            this.Testing.AutoSize = true;
-            this.Testing.Location = new System.Drawing.Point(244, 65);
-            this.Testing.Name = "Testing";
-            this.Testing.Size = new System.Drawing.Size(47, 13);
-            this.Testing.TabIndex = 12;
-            this.Testing.TabStop = true;
-            this.Testing.Text = "Zapnout";
-            this.Testing.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Testing_LinkClicked);
             // 
             // tab
             // 
@@ -113,36 +66,34 @@
             this.tab.Controls.Add(this.tabSetting);
             this.tab.Controls.Add(this.tabLicence);
             this.tab.Controls.Add(this.tabAbout);
-            this.tab.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tab.Location = new System.Drawing.Point(0, 114);
+            this.tab.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tab.Location = new System.Drawing.Point(0, 0);
             this.tab.Name = "tab";
             this.tab.SelectedIndex = 0;
-            this.tab.Size = new System.Drawing.Size(487, 226);
+            this.tab.Size = new System.Drawing.Size(487, 232);
             this.tab.TabIndex = 13;
             this.tab.Selected += new System.Windows.Forms.TabControlEventHandler(this.tab_Selected);
             // 
             // tabProgram
             // 
-            this.tabProgram.Controls.Add(this.actionHelper);
-            this.tabProgram.Controls.Add(this.actionProgress);
+            this.tabProgram.Controls.Add(this.countWords);
             this.tabProgram.Controls.Add(this.listBox2);
             this.tabProgram.Controls.Add(this.tableLayoutPanel1);
             this.tabProgram.Controls.Add(this.spustit);
             this.tabProgram.Location = new System.Drawing.Point(4, 22);
             this.tabProgram.Name = "tabProgram";
             this.tabProgram.Padding = new System.Windows.Forms.Padding(3);
-            this.tabProgram.Size = new System.Drawing.Size(479, 200);
+            this.tabProgram.Size = new System.Drawing.Size(479, 206);
             this.tabProgram.TabIndex = 2;
             this.tabProgram.Text = "Program";
             this.tabProgram.UseVisualStyleBackColor = true;
             // 
             // listBox2
             // 
-            this.listBox2.Dock = System.Windows.Forms.DockStyle.Right;
             this.listBox2.FormattingEnabled = true;
             this.listBox2.Location = new System.Drawing.Point(329, 3);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(147, 194);
+            this.listBox2.Size = new System.Drawing.Size(147, 134);
             this.listBox2.TabIndex = 2;
             // 
             // tableLayoutPanel1
@@ -181,7 +132,7 @@
             // 
             // spustit
             // 
-            this.spustit.Location = new System.Drawing.Point(6, 169);
+            this.spustit.Location = new System.Drawing.Point(6, 114);
             this.spustit.Name = "spustit";
             this.spustit.Size = new System.Drawing.Size(150, 23);
             this.spustit.TabIndex = 0;
@@ -196,7 +147,7 @@
             this.tabSetting.Location = new System.Drawing.Point(4, 22);
             this.tabSetting.Name = "tabSetting";
             this.tabSetting.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSetting.Size = new System.Drawing.Size(479, 200);
+            this.tabSetting.Size = new System.Drawing.Size(479, 206);
             this.tabSetting.TabIndex = 1;
             this.tabSetting.Text = "Nastavení";
             this.tabSetting.UseVisualStyleBackColor = true;
@@ -210,7 +161,7 @@
             this.tabLicence.Location = new System.Drawing.Point(4, 22);
             this.tabLicence.Name = "tabLicence";
             this.tabLicence.Padding = new System.Windows.Forms.Padding(3);
-            this.tabLicence.Size = new System.Drawing.Size(479, 200);
+            this.tabLicence.Size = new System.Drawing.Size(479, 206);
             this.tabLicence.TabIndex = 4;
             this.tabLicence.Text = "Licence";
             // 
@@ -270,39 +221,40 @@
             this.tabAbout.Location = new System.Drawing.Point(4, 22);
             this.tabAbout.Name = "tabAbout";
             this.tabAbout.Padding = new System.Windows.Forms.Padding(3);
-            this.tabAbout.Size = new System.Drawing.Size(479, 200);
+            this.tabAbout.Size = new System.Drawing.Size(479, 206);
             this.tabAbout.TabIndex = 3;
             this.tabAbout.Text = "Informace";
             this.tabAbout.UseVisualStyleBackColor = true;
             // 
-            // actionProgress
+            // statusBar
             // 
-            this.actionProgress.Location = new System.Drawing.Point(162, 169);
-            this.actionProgress.Name = "actionProgress";
-            this.actionProgress.Size = new System.Drawing.Size(161, 23);
-            this.actionProgress.Step = 5;
-            this.actionProgress.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
-            this.actionProgress.TabIndex = 4;
-            this.actionProgress.Visible = false;
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionHelper,
+            this.actionProgress});
+            this.statusBar.Location = new System.Drawing.Point(0, 235);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(487, 22);
+            this.statusBar.SizingGrip = false;
+            this.statusBar.TabIndex = 14;
+            this.statusBar.Text = "status";
             // 
             // actionHelper
             // 
-            this.actionHelper.BackColor = System.Drawing.SystemColors.Info;
-            this.actionHelper.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.actionHelper.Location = new System.Drawing.Point(6, 143);
             this.actionHelper.Name = "actionHelper";
-            this.actionHelper.Size = new System.Drawing.Size(317, 23);
-            this.actionHelper.TabIndex = 5;
-            this.actionHelper.Text = "Popis akce";
-            this.actionHelper.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.actionHelper.Visible = false;
+            this.actionHelper.Size = new System.Drawing.Size(205, 17);
+            this.actionHelper.Text = "Zadejte písmena, a klikněte na Spustit";
+            // 
+            // actionProgress
+            // 
+            this.actionProgress.Name = "actionProgress";
+            this.actionProgress.Size = new System.Drawing.Size(100, 16);
             // 
             // uiNastaveni
             // 
             this.uiNastaveni.Dock = System.Windows.Forms.DockStyle.Fill;
             this.uiNastaveni.Location = new System.Drawing.Point(3, 3);
             this.uiNastaveni.Name = "uiNastaveni";
-            this.uiNastaveni.Size = new System.Drawing.Size(473, 194);
+            this.uiNastaveni.Size = new System.Drawing.Size(473, 200);
             this.uiNastaveni.TabIndex = 0;
             // 
             // autor
@@ -310,20 +262,25 @@
             this.autor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.autor.Location = new System.Drawing.Point(3, 3);
             this.autor.Name = "autor";
-            this.autor.Size = new System.Drawing.Size(473, 194);
+            this.autor.Size = new System.Drawing.Size(473, 200);
             this.autor.TabIndex = 0;
+            // 
+            // countWords
+            // 
+            this.countWords.Location = new System.Drawing.Point(326, 140);
+            this.countWords.Name = "countWords";
+            this.countWords.Size = new System.Drawing.Size(147, 20);
+            this.countWords.TabIndex = 3;
+            this.countWords.Text = "Nalezených slov: 0";
+            this.countWords.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 340);
+            this.ClientSize = new System.Drawing.Size(487, 257);
+            this.Controls.Add(this.statusBar);
             this.Controls.Add(this.tab);
-            this.Controls.Add(this.Testing);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.letters);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.run);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -338,6 +295,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.tabAbout.ResumeLayout(false);
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -345,11 +304,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button run;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox letters;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.LinkLabel Testing;
         private System.Windows.Forms.TabControl tab;
         private System.Windows.Forms.TabPage tabProgram;
         private System.Windows.Forms.TabPage tabSetting;
@@ -367,8 +321,10 @@
         private System.Windows.Forms.Button validateLicence;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ProgressBar actionProgress;
-        private System.Windows.Forms.Label actionHelper;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel actionHelper;
+        private System.Windows.Forms.ToolStripProgressBar actionProgress;
+        private System.Windows.Forms.Label countWords;
     }
 }
 
