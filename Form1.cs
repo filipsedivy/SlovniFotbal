63c93dd4749d5745ff77d2f8e10a1e474cc7e4f2
Filip Sedivy <mail@filipsedivy.cz>
2015-01-29 13:26:18 +0100

    Převedení kódu do třídy

--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,7 @@ namespace Slovní_fotbal
         private Pamet pamet = new Pamet();
         private Sifrovani sifrovano = new Sifrovani();
         private Licence licence = new Licence();
+        private Automaticky automaticky = new Automaticky();
         private Mys mys = new Mys();
 
         public Form1()
@@ -46,35 +47,6 @@ namespace Slovní_fotbal
             learn.Show();
         }
 
-        private string GetPosition(bool button, int dress = 0)
-        {
-            string[] Lines = File.ReadAllLines(licence.getKey("position"));
-            if (button == true)
-            {
-                return Lines[11];
-            }
-            else
-            {
-                return Lines[dress - 1];
-            }
-        }
-
-        private int GetLetterPosition(string letter)
-        {
-            string _letter = letter.ToLower();
-            char[] _letters = letters.Text.ToLower().ToCharArray();
-            int dress = 0;
-            foreach (char _let in _letters)
-            {
-                if (_let.ToString() == _letter)
-                {
-                    return dress + 1;
-                }
-                dress++;
-            }
-            return 0;
-        }
-
         private void Testing_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             if (!licence.isFull())
@@ -83,24 +55,7 @@ namespace Slovní_fotbal
                 Environment.Exit(0);
             }
 
-            foreach (var Item in listBox1.Items)
-            {
-                char[] slovo = Item.ToString().ToCharArray();
-
-                foreach (char _pismeno in slovo)
-                {
-                    int getLetter = GetLetterPosition(_pismeno.ToString());
-                    string[] _Pozice = GetPosition(false, getLetter).Split(";".ToCharArray());
-                    int X = int.Parse(_Pozice[0]);
-                    int Y = int.Parse(_Pozice[1]);
-                    mys.Klikni(Mys.Tlacitko.Leve, X, Y);
-                    System.Threading.Thread.Sleep(200);
-                }
-                string[] _PoziceBu = GetPosition(true, 0).Split(";".ToCharArray());
-                //LeftMouseClick(int.Parse(_PoziceBu[0]), int.Parse(_PoziceBu[1]));
-                mys.Klikni(Mys.Tlacitko.Leve, int.Parse(_PoziceBu[0]), int.Parse(_PoziceBu[1]));
-                System.Threading.Thread.Sleep(100);
-            }
+            automaticky.klikat(textBox1.Text, listBox2);
         }
 
         private void tab_Selected(object sender, TabControlEventArgs e)
@@ -113,11 +68,27 @@ namespace Slovní_fotbal
 
         private void spustit_Click(object sender, EventArgs e)
         {
-            if (!File.Exists(licence.getKey("dictonary")))
+            actionHelper.Visible = true;
+            actionHelper.BackColor = System.Drawing.SystemColors.Info;
+            actionProgress.Visible = true;
+            actionProgress.Style = ProgressBarStyle.Marquee;
+            actionProgress.Value = 0;
+            spustit.Enabled = false;
+
+            actionHelper.Text = "Probíhá vyhledávání slov";
+            try
             {
-                MessageBox.Show("Nemáte zvolen slovník!", "Chyba aplikace");
-                tab.SelectedTab = tabSetting;
-                return;
+                vyhledavac.VyhledatSlova(licence.getKey("dictonary"), textBox1.Text, listBox2);
+            }
+            catch (SlovniFotbalException sE)
+            {
+                actionHelper.Text = sE.Message;
+                actionHelper.BackColor = System.Drawing.Color.LightCoral;
+                actionProgress.Style = ProgressBarStyle.Blocks;
+                actionProgress.Value = 100;
+            }
+            finally {
+                spustit.Enabled = true;
             }
         }
 
