29b9111abce505b3bd20be11fe91fc8778e6c1ea
Filip Sedivy <mail@filipsedivy.cz>
2015-04-18 01:45:47 +0200

    Vytvořen kompilátor, pro ty co nemají nainstalované Visual Studio

---
+++ Compilator.bat
@@ -0,0 +1,46 @@
+@echo off
+
+:: == Promenne ==
+set msBuild=%systemroot%\Microsoft.NET\Framework\v4.0.30319
+set webNet=http://www.microsoft.com/cs-cz/download/details.aspx?id=17851
+
+:: Nazev projektu, ktery se ma kompilovat
+set project=SlovniFotbal
+
+:: Vystupni slozka
+set outputPath=%USERPROFILE%\Desktop\%project%
+
+:: Nemazat ladici informace - pouze pro vyvojare (true|false)
+set debug=false
+
+
+:: == Kompilator ==
+:: Evereni existence slozky
+IF NOT EXIST "%msBuild%" GOTO compilerNotExists
+ELSE GOTO compilerExists
+
+:: Navestidlo, existence .NET
+:compilerExists
+
+:: Vzdalene volani kompileru
+call %msBuild%\msbuild.exe %project%.sln /p:Configuration=Release /p:OutputPath=%outputPath% /l:FileLogger,Microsoft.Build.Engine;logfile=MSBuild_ReleaseCompile.log
+
+:: Odstraneni ladicich informaci
+if %debug% == false (
+del %USERPROFILE%\Desktop\%project%\%project%.pdb
+del MSBuild_ReleaseCompile.log
+)
+
+:: Zobrazeni slozky
+start %USERPROFILE%\Desktop\%project%
+exit
+
+:: Navestidlo, neexistince .NET
+:compilerNotExists
+
+:: Zobrazeni hlasky pomoci JavaScriptu
+mshta javascript:alert("Nemate nainstalovan .NET Framework v4.0\n\nBude otevren webovy prohlizec");close();
+
+:: Zobrazeni webove stranky ke stazeni .NET Frameworku
+start %webNet%
+exit
