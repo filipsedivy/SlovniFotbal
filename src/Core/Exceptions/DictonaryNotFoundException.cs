509b9edb6a327e2b6be124cc44f3aa8fd970ad11
Filip Sedivy <mail@filipsedivy.cz>
2015-04-18 22:46:37 +0200

    Vytvoření vyjímek

---
+++ src/Core/Exceptions/DictonaryNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlovniFotbal.Core.Exceptions
+{
+    class DictonaryNotFoundException : Exception
+    {
+        public DictonaryNotFoundException(string message)
+            : base(message)
+        { }
+
+        public DictonaryNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        { }
+    }
+}
---
+++ src/Core/Exceptions/TextShortException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlovniFotbal.Core.Exceptions
+{
+    class TextShortException : Exception
+    {
+        public TextShortException(string message)
+            : base(message)
+        { }
+
+        public TextShortException(string message, Exception inner)
+            : base(message, inner)
+        { }
+    }
+}
