53076db7e8941a050aa0857cf241c8ee7f3867e4
Filip Sedivy <mail@filipsedivy.cz>
2015-04-18 22:50:04 +0200

    Úprava informací o programu

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Slovní fotbal")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automatický klikač pro hru Slovní fotbal od Hry.cz")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Filip Šedivý")]
 [assembly: AssemblyProduct("Slovní fotbal")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("© Filip Šedivý, 2015")]
+[assembly: AssemblyTrademark("Filip Šedivý")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.5")]
-[assembly: AssemblyFileVersion("1.0.1.8")]
+[assembly: AssemblyVersion("1.0.1.7")]
+[assembly: AssemblyFileVersion("1.0.1.9")]
