418bb5001e57a2322256a292dd11c8ec4044e29f
Filip Sedivy <mail@filipsedivy.cz>
2015-04-20 10:28:19 +0200

    Úprava názvu prvků

--- src/Core/UI/Author.Designer.cs
+++ src/Core/UI/Author.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.licenceLabel = new System.Windows.Forms.Label();
             this.showGitHub = new System.Windows.Forms.LinkLabel();
+            this.licence = new System.Windows.Forms.Label();
+            this.ownWebsite = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,16 +49,15 @@
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 3);
-            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ownWebsite, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.versionLabel, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.licence, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.licenceLabel, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.showGitHub, 1, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
@@ -78,17 +77,6 @@
             this.label1.Text = "Autor";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(84, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(197, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Filip Šedivý";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -100,50 +88,26 @@
             this.label3.Text = "Kontakt";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // linkLabel1
+            // versionLabel
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.linkLabel1.Location = new System.Drawing.Point(84, 25);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(197, 25);
-            this.linkLabel1.TabIndex = 3;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "http://filipsedivy.cz";
-            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.versionLabel.Location = new System.Drawing.Point(3, 75);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(75, 25);
+            this.versionLabel.TabIndex = 4;
+            this.versionLabel.Text = "Verze";
+            this.versionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label4
+            // licenceLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label4.Location = new System.Drawing.Point(3, 75);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 25);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Verze";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label5.Location = new System.Drawing.Point(84, 75);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(197, 25);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "label5";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label6
-            // 
-            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label6.Location = new System.Drawing.Point(3, 50);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(75, 25);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Licence";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.licenceLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.licenceLabel.Location = new System.Drawing.Point(3, 50);
+            this.licenceLabel.Name = "licenceLabel";
+            this.licenceLabel.Size = new System.Drawing.Size(75, 25);
+            this.licenceLabel.TabIndex = 6;
+            this.licenceLabel.Text = "Licence";
+            this.licenceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // showGitHub
             // 
@@ -155,14 +119,49 @@
             this.showGitHub.TabStop = true;
             this.showGitHub.Text = "OpenSource GNU - GitHub";
             this.showGitHub.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.showGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.showGitHub_LinkClicked);
+            this.showGitHub.Click += new System.EventHandler(this.OpenBrowser);
             // 
-            // Autor
+            // licence
+            // 
+            this.licence.AutoSize = true;
+            this.licence.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.licence.Location = new System.Drawing.Point(84, 75);
+            this.licence.Name = "licence";
+            this.licence.Size = new System.Drawing.Size(197, 25);
+            this.licence.TabIndex = 5;
+            this.licence.Text = "label5";
+            this.licence.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ownWebsite
+            // 
+            this.ownWebsite.AutoSize = true;
+            this.ownWebsite.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ownWebsite.Location = new System.Drawing.Point(84, 25);
+            this.ownWebsite.Name = "ownWebsite";
+            this.ownWebsite.Size = new System.Drawing.Size(197, 25);
+            this.ownWebsite.TabIndex = 3;
+            this.ownWebsite.TabStop = true;
+            this.ownWebsite.Text = "http://filipsedivy.cz";
+            this.ownWebsite.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ownWebsite.Click += new System.EventHandler(this.OpenBrowser);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(84, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Filip Šedivý";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Author
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Autor";
+            this.Name = "Author";
             this.Size = new System.Drawing.Size(284, 170);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -174,12 +173,12 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.Label licenceLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel ownWebsite;
+        private System.Windows.Forms.Label licence;
         private System.Windows.Forms.LinkLabel showGitHub;
     }
 }
